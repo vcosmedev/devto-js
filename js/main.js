d7214b1f00d3002175ebf7456fc96fcdf1cb9b34
unknown <manueloliverms@hotmail.com>
2023-07-30 05:05:04 -0600

    [add: estructura aside final ]

--- js/main.js
+++ js/main.js
@@ -5,38 +5,31 @@ import { orderData } from './modules/orders.js';
 const authorpic = document.getElementById('author-picture');
 authorpic.src = localStorage.getItem('image');
 authorpic.classList.remove('d-none');
-//variable global del array de post
+
+// Función para obtener los datos de los posts
 const processData = async () => {
   const dataposts = await getPosts();
-
   const keys = Object.keys(dataposts);
-
   const array = keys.reduce((accum, key) => {
     const currobj = dataposts[key];
-
     currobj['id'] = key;
-
     return [...accum, currobj];
   }, []);
-
   return array;
 };
 
 const data = await processData();
 
-
+// Función para renderizar los posts principales
 const main = document.getElementById('cards-main');
 
 const renderData = (array) => {
   let count = 0;
-
   array.forEach((post) => {
     let isfirst = false;
     count === 0 && (isfirst = true);
     count++;
-
     const cardpost = createPost(post, isfirst);
-
     main.appendChild(cardpost);
   });
 };
@@ -47,24 +40,26 @@ const cleanMain = () => {
   main.innerHTML = '';
 };
 
+// Manejo del filtrado de los posts
 const order = document.querySelectorAll('.data-item');
 let orderactive = document.querySelector('.main__title__selected');
-let curentdata;
+let currentData;
 
 order.forEach((item) => {
   item.addEventListener('click', ({ target }) => {
     if (orderactive !== item) {
       orderactive.classList.remove('main__title__selected');
       item.classList.add('main__title__selected');
-      curentdata = orderData(data, target.id);
+      currentData = orderData(data, target.id);
 
       cleanMain();
-      renderData(curentdata);
+      renderData(currentData);
 
       orderactive = item;
     }
   });
 });
+
 
 const createCard = (post) => {
   const card = document.createElement('div');
@@ -77,45 +72,107 @@ const createCard = (post) => {
   cardTitle.classList.add('card-title', 'd-flex', 'justify-content-between');
   cardTitle.textContent = post.title;
 
-  const separator = document.createElement('hr');
-  separator.classList.add('my-2');
-
   cardBody.appendChild(cardTitle);
-  cardBody.appendChild(separator);
 
-  const tags = post.tags.filter(tag => tag === 'discuss' || tag === 'watercooler');
-  const tagsContainer = document.createElement('div');
-  tagsContainer.textContent = `Tags: ${tags.join(', ')}`;
-
-  const readTime = document.createElement('div');
-  readTime.textContent = `Read Time: ${post.readtime} min`;
-
+  // Contenedor de comentarios
   const commentsContainer = document.createElement('div');
-  const commentsLink = document.createElement('a');
-  commentsLink.classList.add('comment__vinc');
-  commentsLink.href = '#';
-  commentsLink.textContent = `${post.comments.length} Comentarios`;
+  let commentsCount = 0;
 
-  commentsContainer.appendChild(commentsLink);
+  if (post.comments && Array.isArray(post.comments)) {
+    commentsCount = post.comments.length;
+  }
 
-  cardBody.appendChild(tagsContainer);
-  cardBody.appendChild(readTime);
+  const commentsText = commentsCount === 1 ? `${commentsCount} Comentario` : `${commentsCount} Comentarios`;
+  commentsContainer.textContent = commentsText;
+
   cardBody.appendChild(commentsContainer);
 
   card.appendChild(cardBody);
   return card;
 };
 
-// Función para renderizar el aside
+
+
+
+
+
+
 const renderAside = (data) => {
   const asideContainer = document.getElementById('asideContainer');
 
-  const filteredPosts = data.filter(post => post.tags.includes('#discuss') || post.tags.includes('#watercooler'));
-  filteredPosts.forEach((post) => {
-    const card = createCard(post);
-    asideContainer.appendChild(card);
+  // Limpiamos el aside para evitar duplicados
+  asideContainer.innerHTML = '';
+
+  // Creamos una lista para "Discuss"
+  const discussList = document.createElement('ul');
+  discussList.classList.add('list-group', 'mb-4');
+
+  const discussTitle = document.createElement('li');
+  discussTitle.classList.add('list-group-item', 'fw-bold');
+  discussTitle.textContent = '#discuss';
+  discussList.appendChild(discussTitle);
+
+  // Creamos una lista para "Watercooler"
+  const watercoolerList = document.createElement('ul');
+  watercoolerList.classList.add('list-group', 'mb-4');
+
+  const watercoolerTitle = document.createElement('li');
+  watercoolerTitle.classList.add('list-group-item', 'fw-bold');
+  watercoolerTitle.textContent = '#watercooler';
+  watercoolerList.appendChild(watercoolerTitle);
+
+  // Función para crear un elemento <p> con el texto de comentarios
+  const createCommentsElement = (commentsCount) => {
+    const commentsElement = document.createElement('p');
+    commentsElement.classList.add('mb-0', 'comments-text'); // Agregar la clase "comments-text"
+    const commentsText = commentsCount === 1 ? `${commentsCount} Comentario` : `${commentsCount} Comentarios`;
+    commentsElement.textContent = commentsText;
+    return commentsElement;
+  };
+  
+
+  // Filtramos los posts por las categorías "Discuss" y "Watercooler"
+  const discussPosts = data.filter(post => post.tags.includes('#discuss')).slice(0, 3);
+  const watercoolerPosts = data.filter(post => post.tags.includes('#watercooler')).slice(0, 3);
+
+  // Renderizamos los títulos y comentarios para cada categoría en la lista
+  discussPosts.forEach((post) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-group-item');
+
+    const titleElement = document.createElement('h5');
+    titleElement.textContent = post.title;
+
+    const commentsCount = post.comments ? post.comments.length : 0;
+    const commentsElement = createCommentsElement(commentsCount);
+
+    listItem.appendChild(titleElement);
+    listItem.appendChild(commentsElement);
+
+    discussList.appendChild(listItem);
   });
+
+  watercoolerPosts.forEach((post) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-group-item');
+
+    const titleElement = document.createElement('h5');
+    titleElement.textContent = post.title;
+
+    const commentsCount = post.comments ? post.comments.length : 0;
+    const commentsElement = createCommentsElement(commentsCount);
+
+    listItem.appendChild(titleElement);
+    listItem.appendChild(commentsElement);
+
+    watercoolerList.appendChild(listItem);
+  });
+
+  // Agregamos las listas al aside
+  asideContainer.appendChild(discussList);
+  asideContainer.appendChild(watercoolerList);
 };
+
 
 // Llamamos a la función para renderizar el aside con los post filtrados por los tags
 renderAside(data);
